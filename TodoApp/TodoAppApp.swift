7b8498e5b5c185f4eb40dc52b1dfba196d62d163
f426k <gaodaofujia@gmail.com>
2023-07-19 13:20:32 +0900

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by 高嶋芙佳 on 2023/07/19.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
